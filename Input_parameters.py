9998a4fc3423e31f390e0424ec7264219e241231
Debodipto Roy <debodiptoroy@gmail.com>
2018-02-27 13:04:15 +0530

    Add files via upload

---
+++ Input_parameters.py
@@ -0,0 +1,19 @@
+#Input File
+
+#####################################################
+#Input Meta data file path along with file name (absolute path)
+#####################################################
+
+any_metadata_path = "D:\Scotia\Accelerators\Cognos SemanticLayer Accelerator (CSA)\CSA Release Versions\Demo\CSA_v1.1\Input\TSYS_Metadata.csv"
+
+
+#####################################################
+#Input Project Details
+#####################################################
+#Note : Format of pathibmonline : folder1<space>folder2<space>folder3
+
+project_name= "Dummy_POC_Project"
+project_path= "Output"
+database= "DS_LATAM_BBCV_MIS"
+schema= "bbcv_mis"
+pathibmonline= "LATAM-Christopher EDL-Trial"
